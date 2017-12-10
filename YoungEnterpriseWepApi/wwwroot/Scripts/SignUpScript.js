95d9f100d897403876f977eb03e8b8280b65fce3
zantex94 <reneseer@gmail.com>
2017-12-11 00:03:08 +0100

    tried to make a sign up for fun :)

--- YoungEnterpriseWepApi/wwwroot/Scripts/SignUpScript.js
+++ YoungEnterpriseWepApi/wwwroot/Scripts/SignUpScript.js
@@ -1,1 +1,45 @@
-﻿
+﻿$(document).ready(function () {
+    $("#bCancel").click(function () {
+        window.location = "LoginPage.html";
+    });
+    $("#bCreateAccount").click(function () {
+        var dummyPerson = { firstName: $("#tfFirstName").val(), lastName: $("#tflastName").val(), mail: $("#tfMail").val()}
+        var passWord = $("#tfPassword").val();
+        var passCon = $("#tfConfirmPassword").val();
+
+        if (!(passWord === passCon)) {
+            alert("Passwords do not match!");
+            return;
+        }
+
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:8419/api/people",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(dummyPerson),
+            success: function (data) {
+                var account = { userName: $("#tfUserName").val(), userPassword: passWord, personId: data["personId"] };
+
+                $.ajax({
+                    method: "POST",
+                    url: "http://localhost:8419/api/accounts",
+                    dataType: "json",
+                    contentType: "application/json",
+                    data: JSON.stringify(account),
+                    success: function (data) {
+                        alert("account created!");
+                    },
+                    error: function (data) {
+                        alert(JSON.stringify(data));
+                    }
+                });
+            },
+            error: function (data) {
+                alert(JSON.stringify(data));
+            }
+        });
+    });
+});
+
+});
